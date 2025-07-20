d7fd670dcf7554586265552c1dcf73b79248c168
M11401006 <M11401006@ntust.edu.tw>
2025-07-20 19:20:27 +0800

    revise the py code

--- pratice.py
+++ pratice.py
@@ -4,8 +4,7 @@ load_dotenv()
 
 api_KEY = os.getenv("API_KEY")
 database_URL = os.getenv("DATABASE_URL")
-secret_KEY = os.getenv("SECRET_KEY")
+
 
 print(api_KEY)
 print(database_URL)
-print(secret_KEY)
